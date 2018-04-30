43e16f6aa263250f067712fced86e55a043552a4
Knguyen14 <keng3875@colorado.edu>
2018-04-29 22:25:00 -0600

    Adding More Stats per SO

--- Project.sql
+++ Project.sql
@@ -1,5 +1,7 @@
 use lahman2016; 
 
-select playerID,teamID, yearID, H, SO,  H/SO as HPerSO from batting where yearID = 2017 order by HPerSO DESC;
+select playerID,teamID, yearID, H, SO, AB, H/SO as HPerSO, RBI/SO as RBIPerSO , R/SO as RPerSO from batting where yearID = 2017 and AB > 200 order by HPerSO DESC;
 
-select * from batting where yearID = 2017 and teamID = 'COL';
+select * from batting where yearID = 2017;
+select * from batting where teamID = 'COL';
+select * from salaries;
