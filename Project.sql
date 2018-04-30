410b465e12b61315a6ae356a3c951abca67d9e9a
Knguyen14 <keng3875@colorado.edu>
2018-04-29 22:27:25 -0600

    Add SO per AB

--- Project.sql
+++ Project.sql
@@ -1,6 +1,6 @@
 use lahman2016; 
 
-select playerID,teamID, yearID, H, SO, AB, H/SO as HPerSO, RBI/SO as RBIPerSO , R/SO as RPerSO from batting where yearID = 2017 and AB > 200 order by HPerSO DESC;
+select playerID,teamID, yearID, H, SO, AB, H/SO as HPerSO, RBI/SO as RBIPerSO , R/SO as RPerSO, SO/AB as KPerAB from batting where yearID = 2017 and AB > 200 order by KPerAB DESC;
 
 select * from batting where yearID = 2017;
 select * from batting where teamID = 'COL';
