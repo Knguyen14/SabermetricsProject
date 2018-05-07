a0484de89ee008bb6641c216435c27c84e372b8d
Knguyen14 <keng3875@colorado.edu>
2018-05-07 08:43:32 -0600

    Uploading Data File and updating sql queries

--- Project.sql
+++ Project.sql
@@ -1,6 +1,10 @@
 use lahman2016; 
 
-select playerID,teamID, yearID, H, SO, AB, H/SO as HPerSO, RBI/SO as RBIPerSO , R/SO as RPerSO, SO/AB as KPerAB from batting where yearID = 2017 and AB > 200 order by KPerAB DESC;
+select b.playerID, m.nameFirst as FirstName, m.nameLast as LastName,b.teamID, b.yearID, b.H,b.HR, b.SO, b.AB, b.H/SO as HPerSO, b.HR/b.SO as HRPerSO, b.RBI/b.SO as RBIPerSO , b.R/b.SO as RPerSO, b.SO/b.AB as KPerAB from batting b join master m on b.playerID = m.playerID 
+where yearID > 2000;
+
+
+select b.playerID, m.nameFirst as FirstName, m.nameLast as LastName,b.teamID, b.yearID, b.H,b.HR, b.SO, b.RBI, b.R, b.AB from batting b join master m on b.playerID = m.playerID where b.yearID >= 2000;
 
 select * from batting where yearID = 2017;
 select * from batting where teamID = 'COL';
