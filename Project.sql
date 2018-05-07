28ecfeb77cc83a1edd50999791202ecc5992944e
Knguyen14 <keng3875@colorado.edu>
2018-05-07 09:55:15 -0600

    SQL update

--- Project.sql
+++ Project.sql
@@ -1,6 +1,7 @@
 use lahman2016; 
 
-select b.playerID, m.nameFirst as FirstName, m.nameLast as LastName,b.teamID, b.yearID, b.H,b.HR,b.R, b.SO, b.AB, b.H/SO as HPerSO, 
-b.HR/b.SO as HRPerSO, b.RBI/b.SO as RBIPerSO , b.R/b.SO as RPerSO, b.SO/b.AB as KPerAB, b.H/b.AB as BA from batting b join master m on b.playerID = m.playerID where yearID = 2017 ;
+select b.playerID, m.nameFirst as FirstName, m.nameLast as LastName,b.teamID, b.yearID, b.H,b.HR,b.R,b.RBI ,b.SO, b.AB, b.H/SO as HPerSO, 
+b.HR/b.SO as HRPerSO, b.RBI/b.SO as RBIPerSO , b.R/b.SO as RPerSO, b.SO/b.AB as KPerAB, b.H/b.AB as BA from batting b join master m 
+on b.playerID = m.playerID where yearID >= 1990 ;
 
 
